6f99b8ee73c0e5813047b08eba4ca0c0d04f30ab
ZxStim <thang.victor.luong@gmail.com>
2024-04-01 12:46:58 +0700

    edit en AdvancedStorage sol

--- en/solidity/4-Advanced/AdvancedStorage.sol
+++ en/solidity/4-Advanced/AdvancedStorage.sol
@@ -3,69 +3,69 @@
 pragma solidity ^0.8.20;
 
 contract AdvancedStorage {
-    // Khai báo địa chỉ của 1 quản lý két
+    // Declare the address of a vault manager
     address public vaultManager;
 
-    // Đây là khai báo lỗi không được truy cập
+    // Declare an error type for unauthorized access
     error OwnableUnauthorizedAccount(address account);
 
-    // Constructor là function sẽ chạy ngay khi contract được khởi tạo
+    // Constructor is a function that runs when the contract is initialized
     constructor() {
-        // Gán địa chỉ của deployer vào biến quản lý két
+        // Assign the address of the deployer to the vault manager variable
         vaultManager = msg.sender;
     }
 
-    // Khai báo định dạng dữ liệu InvestmentVault
+    // Declare the InvestmentVault Struct data type
     struct InvestmentVault {
         uint256 investmentDuration; // Thời gian đầu tư
         int256 returnOnInvestment; // % lãi suất trả về
         bool initialized; // Đã khởi tạo
         address identityCard; // Địa chỉ thẻ thông tin
     }
-    // Khai báo 1 biến có định dạng InvestmentVault
+    // Declare a variable with the InvestmentVault type
     InvestmentVault private investmentVault;
 
-    // Function khởi tạo InvestmentVault
+    // This function initializes the investment vault
     function setInitialInvestmentVault(uint256 daysAfter, int256 _returnOnInvestment, address _vaultOwner) public {
-        // Chúng ta kiểm tra người khởi tạo có phải là vaultManager
+        // We check if the initiator is the vaultManager
         if (msg.sender != vaultManager) {
-            // Keyword revert sẽ huỷ toàn bộ các action đã thực hiện và revert/quay ngược lại giao dịch
+            // This reverts all actions and reverts the transaction
             revert OwnableUnauthorizedAccount(msg.sender);
         }
-        // Khai báo thời gian đầu tư
+        // Declare the investment duration
         uint256 _investmentDuration = block.timestamp + daysAfter * 1 days;
 
-        // Tạo ra 1 identity card mới cho khách hàng
+        // Create a new identity card for the customer
         CustomerIdentityCard customerIdentityCard = new CustomerIdentityCard(_vaultOwner);
-        // Gán địa chỉ ví của chủ vault/khách hàng vào mapping với thông tin của vault
+        // Assign the address of the vault owner/customer to the mapping with the vault information
         investmentVault = InvestmentVault({investmentDuration: _investmentDuration, returnOnInvestment: _returnOnInvestment, initialized: true, identityCard: address(customerIdentityCard)});
     }
 
-    // Hàm thay đổi lãi suất
+    // Function to change the return on investment
     function editReturnOnInvestment(int256 _newReturnOnInvestment) public {
-        // keyword require hoạt động tương tự như if và revert ở trên
+        // require keyword works similarly to if and revert above
         require (msg.sender == vaultManager, "Unauthorized Manager");
-        // Thay đổi giá trị lãi suất
+        // Change the value of the interest rate
         investmentVault.returnOnInvestment = _newReturnOnInvestment;
     }
 
-    // Hàm trả về thông tin investmentVault
+    // Function to return investmentVault information
     function retrieveInvestmentVault() public view returns (InvestmentVault memory _investmentVault) {
         return investmentVault;
     }
 
-    // Hàm trả về address của IdentityCard
+    // Function to return the address of the IdentityCard
     function retrieveCustomerInformation() public view returns (address) {
         return CustomerIdentityCard(investmentVault.identityCard).customer();
     }
 }
 
-// Contract khai báo địa chỉ của chủ vault
+// Contract that stores the address of the vault owner
 contract CustomerIdentityCard {
-    // khai báo 1 địa chỉ customer
+    //  declares a variable to store the address of the customer
     address public customer;
 
-    // khởi tạo contract và cho vào địa chỉ của customer
+    // initialize the contract and assign the address of the customer
     constructor(address _customer) {
         customer = _customer;
     }
